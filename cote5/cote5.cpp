dc7aea2aa89ddddd984e32ddf232d6770fbafa6e
ohne <ghdlr785@gmail.com>
2022-06-17 09:32:39 +0900

    초기 커밋.

---
+++ cote5/cote5.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <math.h>
+
+using namespace std;
+
+double solution(vector<int> arr) {
+    double answer = 0.0;
+    double i = 0.0 ;
+    for ( i = 0; i < arr.size(); i++)
+    {
+        answer += arr[i];
+    }
+    
+    answer /= i;
+    cout << answer << endl;
+    return answer;
+}
+
+int main()
+{
+    vector <int> a = { 1,2,3,4 };
+    solution(a);
+    
+}
+
